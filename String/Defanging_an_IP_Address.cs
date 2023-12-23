3c9138cffca1456c53978a1ce34e7bdbdf3979b5
Sardoralgoritm <sardorsaminov5@gmail.com>
2023-12-23 23:25:01 +0500

    Added Defanging an IP Address problem

---
+++ String/Defanging_an_IP_Address.cs
@@ -0,0 +1,17 @@
+﻿/// 1108. https://leetcode.com/problems/defanging-an-ip-address/
+
+using System.Text;
+namespace String;
+public class Solution
+{
+    public string DefangIPaddr(string address)
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (char i in address)
+        {
+            if (i == '.') result.Append("[.]");
+            else result.Append(i);
+        }
+        return result.ToString();
+    }
+}
