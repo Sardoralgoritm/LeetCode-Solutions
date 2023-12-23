4807133206455bcd370440546fb96778e5250006
Sardoralgoritm <sardorsaminov5@gmail.com>
2023-12-23 23:06:39 +0500

    Add Build Array from Permutation problem

---
+++ Array/Concatenation_of_Array.cs
@@ -0,0 +1,17 @@
+﻿/// 1920. https://leetcode.com/problems/build-array-from-permutation/description/
+
+namespace Array;
+public class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int n = nums.Length;
+        int[] ans = new int[n * 2];
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
