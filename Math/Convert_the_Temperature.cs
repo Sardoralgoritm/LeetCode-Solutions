22ca6461d6f86ca00624c78c50ff5575997e36be
Sardoralgoritm <sardorsaminov5@gmail.com>
2023-12-23 23:44:55 +0500

    Added Convert the Temperature problem

---
+++ Math/Convert_the_Temperature.cs
@@ -0,0 +1,10 @@
+﻿/// 2469. https://leetcode.com/problems/convert-the-temperature/description/
+
+namespace Math;
+public class Solution
+{
+    public double[] ConvertTemperature(double celsius)
+    {
+        return new double[] { celsius + 273.15d, celsius * 1.80d + 32.00d };
+    }
+}
