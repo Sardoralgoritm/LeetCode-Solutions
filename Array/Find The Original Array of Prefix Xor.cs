6b02699958021b3db79a0115239bf91bdb90985e
Sardoralgoritm <sardorsaminov5@gmail.com>
2023-12-24 11:12:35 +0500

    Added Find The Original Array of Prefix Xor problem

---
+++ Array/Find The Original Array of Prefix Xor.cs
@@ -0,0 +1,18 @@
+﻿/// 2433. https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+
+namespace Array;
+
+public class Solution
+{
+    public int[] FindArray(int[] pref)
+    {
+        int n = pref.Length;
+        int[] result = new int[n];
+        result[0] = pref[0];
+        for (int i = 1; i < n; i++)
+        {
+            result[i] = pref[i] ^ pref[i - 1];
+        }
+        return result;
+    }
+}
