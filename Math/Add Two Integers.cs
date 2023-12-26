48e03a25b880d92861f0c4690d1f78e0f4d6f559
Sardoralgoritm <sardorsaminov5@gmail.com>
2023-12-26 09:08:36 +0500

    Added Add Two Integers problem

---
+++ Math/Add Two Integers.cs
@@ -0,0 +1,11 @@
+﻿/// 2235. https://leetcode.com/problems/add-two-integers/
+
+namespace Math;
+
+public class Solution
+{
+    public int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
