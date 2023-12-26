a851c45d8e55498e7bd8280fb491b4e61a7724c9
Sardoralgoritm <sardorsaminov5@gmail.com>
2023-12-26 09:59:47 +0500

    Added Encode and Decode TinyURL problem

---
+++ Design/Encode and Decode TinyURL.cs
@@ -0,0 +1,20 @@
+﻿/// 535. https://leetcode.com/problems/encode-and-decode-tinyurl/
+
+
+namespace Design;
+
+public class Codec
+{
+
+    // Encodes a URL to a shortened URL
+    public string encode(string longUrl)
+    {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public string decode(string shortUrl)
+    {
+        return shortUrl;
+    }
+}
