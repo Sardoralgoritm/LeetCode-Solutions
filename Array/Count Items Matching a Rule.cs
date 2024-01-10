1bf51bc2551a4763add71f7951ea7715ce95e4e0
sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-10 12:57:59 +0500

    Added Count Items Matching a Ruleproblem's solution

---
+++ Array/Count Items Matching a Rule.cs
@@ -0,0 +1,28 @@
+﻿/// 1773. https://leetcode.com/problems/count-items-matching-a-rule/
+
+namespace Array;
+
+public class Solution
+{
+    public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+    {
+        int count = 0;
+        for (int i = 0; i < items.Count(); i++)
+        {
+            var item = items[i];
+            if (ruleKey == "type")
+            {
+                if (item[0] == ruleValue) count++;
+            }
+            else if (ruleKey == "color")
+            {
+                if (item[1] == ruleValue) count++;
+            }
+            else
+            {
+                if (item[2] == ruleValue) count++;
+            }
+        }
+        return count;
+    }
+}
