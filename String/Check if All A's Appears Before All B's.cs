329d057cab6f80009c500884ed66dbcd61762b04
sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-10 16:03:54 +0500

    Added Check if All A's Appears Before All B's problem's solution

---
+++ String/Check if All A's Appears Before All B's.cs
@@ -0,0 +1,32 @@
+﻿// 2124. https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/
+
+namespace String;
+
+public class Solution
+{
+    public bool CheckString(string s)
+    {
+        int indexA = FindLastIndex(s, 'a');
+        int indexB = FindIndex(s, 'b');
+        return (indexB == -1 || indexA < indexB) ? true : false;
+    }
+
+    private int FindIndex(string s, char a)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == a) return i;
+        }
+        return -1;
+    }
+
+    private int FindLastIndex(string s, char a)
+    {
+        int index = s.Length - 1;
+        for (int i = index; i >= 0; i--)
+        {
+            if (s[i] == a) return i;
+        }
+        return -1;
+    }
+}
