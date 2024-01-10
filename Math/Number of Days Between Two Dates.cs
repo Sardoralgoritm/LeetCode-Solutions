a8ea0abd4795c152f5614c2e8b28c300c0e7eb6f
sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-10 16:22:16 +0500

    Added Number of Days Between Two Dates problem's solution

---
+++ Math/Number of Days Between Two Dates.cs
@@ -0,0 +1,14 @@
+﻿// 1360. https://leetcode.com/problems/number-of-days-between-two-dates/
+
+namespace Math;
+
+public class Solution
+{
+    public int DaysBetweenDates(string date1, string date2)
+    {
+        DateTime dateTime1 = DateTime.Parse(date1);
+        DateTime dateTime2 = DateTime.Parse(date2);
+        TimeSpan different = dateTime1 - dateTime2;
+        return Math.Abs(different.Days);
+    }
+}
