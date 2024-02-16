f1d09a3eb3ed85fc879c66c029dea4131c83556b
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-16 08:38:17 +0500

    2149 - solution and 1481 - solution added

---
+++ Array/Rearrange Array Elements by Sign.cs
@@ -0,0 +1,33 @@
+﻿/// 2149. https://leetcode.com/problems/rearrange-array-elements-by-sign/
+
+namespace Array;
+
+public class Solution
+{
+    public int[] RearrangeArray(int[] nums)
+    {
+        var items = Helper(nums);
+        var positive = items.positive;
+        var negative = items.negative;
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Length / 2; i++)
+        {
+            result.Add(positive[i]);
+            result.Add(negative[i]);
+        }
+
+        return result.ToArray();
+    }
+
+    public (int[] positive, int[] negative) Helper(int[] arr)
+    {
+        List<int> pos = new List<int>();
+        List<int> neg = new List<int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] >= 0) pos.Add(arr[i]);
+            else neg.Add(arr[i]);
+        }
+        return (pos.ToArray(), neg.ToArray());
+    }
+}
---
+++ Hash Table/Least Number of Unique Integers after K Removals.cs
@@ -0,0 +1,29 @@
+﻿/// 1481. https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/?envType=daily-question&envId=2024-02-16
+
+namespace Hash_Table;
+
+public class Solution
+{
+    public int FindLeastNumOfUniqueInts(int[] arr, int k)
+    {
+        Dictionary<int, int> countOfNumber = new Dictionary<int, int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (countOfNumber.ContainsKey(arr[i])) countOfNumber[arr[i]]++;
+            else countOfNumber[arr[i]] = 1;
+        }
+
+        var values = countOfNumber.Values.OrderBy(x => x).ToList();
+        int minus = 0;
+        foreach (var i in values)
+        {
+            if (i <= k)
+            {
+                k -= i;
+                minus++;
+            }
+            else break;
+        }
+        return countOfNumber.Count() - minus;
+    }
+}
