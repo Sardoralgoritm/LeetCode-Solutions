2ebf813a905eeddfc08cf534ffc636e97fe27559
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-17 11:51:10 +0500

    121 - problem added

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,27 @@
+﻿/// 121.  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+namespace Dynamic_Programming;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int left = 0;
+        int right = 1;
+        int result = 0;
+
+        while (right < prices.Length)
+        {
+            if (prices[right] >= prices[left])
+            {
+                result = Math.Max(result, prices[right] - prices[left]);
+            }
+            else
+            {
+                left = right;
+            }
+            right++;
+        }
+        return result;
+    }
+}
