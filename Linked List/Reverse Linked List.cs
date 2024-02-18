af4ec828c64537952c32a11bef02a8d5b34fa679
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-18 20:34:54 +0500

    206 - problem Added

---
+++ Linked List/Reverse Linked List.cs
@@ -0,0 +1,33 @@
+﻿/// 206. https://leetcode.com/problems/reverse-linked-list/
+
+
+namespace Linked_List;
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (cur != null)
+        {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
