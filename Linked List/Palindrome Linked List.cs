603eec950ba6763759c38e92cc454de34d31fc82
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-18 22:46:05 +0500

    234 - problem added

---
+++ Linked List/Palindrome Linked List.cs
@@ -0,0 +1,38 @@
+﻿/// 234.  https://leetcode.com/problems/palindrome-linked-list/description/
+
+
+namespace Linked_List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsPalindrome(ListNode head)
+    {
+        Stack<int> stack = new Stack<int>();
+        ListNode dummy = head;
+        if (head == null) return true;
+
+        while (head != null)
+        {
+            stack.Push(head.val);
+            head = head.next;
+        }
+
+        while (dummy != null)
+        {
+            if (dummy.val != stack.Pop()) return false;
+            dummy = dummy.next;
+        }
+        return true;
+    }
+}
