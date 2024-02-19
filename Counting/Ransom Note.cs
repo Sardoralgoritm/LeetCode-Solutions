e9c1b357e2815bc37137af538dfa85f7372c980c
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-19 11:03:55 +0500

    383 - problem added

---
+++ Counting/Ransom Note.cs
@@ -0,0 +1,23 @@
+﻿/// 383. https://leetcode.com/problems/ransom-note/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+namespace Counting;
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        int[] counter = new int[26];
+        foreach (char i in magazine)
+        {
+            counter[i - 'a']++;
+        }
+
+        foreach (char i in ransomNote)
+        {
+            if (counter[i - 'a'] == 0) return false;
+            counter[i - 'a']--;
+        }
+        return true;
+    }
+}
