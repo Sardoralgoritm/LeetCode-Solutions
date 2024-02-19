8336a94f3f846a01178f8be246bf51014279f426
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-19 16:12:23 +0500

    88 - problem added

---
+++ Array/Merge Sorted Array.cs
@@ -0,0 +1,20 @@
+﻿/// 88.  https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+
+
+namespace Array;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i = 0;
+        while (n > 0)
+        {
+            nums1[m] = nums2[i];
+            n--;
+            i++;
+            m++;
+        }
+        Array.Sort(nums1);
+    }
+}
