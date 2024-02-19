d04ce37399eef654a47e511c66a416005b34d2f7
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-19 12:28:13 +0500

    344 - problem added

---
+++ Two Pointers/Reverse String.cs
@@ -0,0 +1,17 @@
+﻿/// 344.  https://leetcode.com/problems/reverse-string/
+
+
+namespace Two_Pointers;
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            char tmp = s[i];
+            s[i] = s[s.Length - i - 1];
+            s[s.Length - i - 1] = tmp;
+        }
+    }
+}
