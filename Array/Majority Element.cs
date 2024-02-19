7a377a58f09ae860b0c627f0fbc6a77e06f17d51
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-19 16:16:17 +0500

    169 - problem added

---
+++ Array/Majority Element.cs
@@ -0,0 +1,13 @@
+﻿/// 169. https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+namespace Array;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        Array.Sort(nums);
+        return nums[nums.Length / 2];
+    }
+}
