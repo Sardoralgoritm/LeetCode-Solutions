3ad038f8c0339001b88ffc972ef51ae26d131d78
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-19 10:02:03 +0500

    231 - problem added

---
+++ Math/Power of Two.cs
@@ -0,0 +1,14 @@
+﻿/// 231.  https://leetcode.com/problems/power-of-two/?envType=daily-question&envId=2024-02-19
+
+
+namespace Math;
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n == 0) return false;
+        while (n % 2 == 0) n /= 2;
+        return n == 1;
+    }
+}
