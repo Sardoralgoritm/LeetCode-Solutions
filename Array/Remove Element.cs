0f0a759529dcbf934de2cd97f9be2e9080adf7dc
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-19 16:03:02 +0500

    27 - problem added

---
+++ Array/Remove Element.cs
@@ -0,0 +1,21 @@
+﻿/// 27.  https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+
+
+namespace Array;
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int index = 0;
+        foreach (int i in nums)
+        {
+            if (i != val)
+            {
+                nums[index] = i;
+                index++;
+            }
+        }
+        return index;
+    }
+}
