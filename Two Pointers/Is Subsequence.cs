b70cdfd4a322ce4aa168e3db5389e743fac2f87d
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-20 15:49:11 +0500

    392nd problem added

---
+++ Two Pointers/Is Subsequence.cs
@@ -0,0 +1,23 @@
+﻿/// 392.  https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+namespace Two_Pointers;
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0 && t.Length == 0) return true;
+
+        if (s.Length == 0) return true;
+
+        int currIndex = 0;
+        int i = 0;
+        while (currIndex < s.Length && i < t.Length)
+        {
+            if (s[currIndex] == t[i]) currIndex++;
+            i++;
+        }
+        return currIndex == s.Length;
+    }
+}
