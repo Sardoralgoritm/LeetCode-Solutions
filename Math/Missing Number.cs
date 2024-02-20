9cbcf5c94fada44afa9ff31e2fc1830e3b9dc4df
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-20 07:14:56 +0500

    268nd problem added

---
+++ Math/Missing Number.cs
@@ -0,0 +1,16 @@
+﻿/// 268.  https://leetcode.com/problems/missing-number/?envType=daily-question&envId=2024-02-20
+
+
+namespace Math;
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        int sumOfNums = (n * (n + 1)) / 2;
+        int sum = 0;
+        foreach (int i in nums) sum += i;
+        return sumOfNums - sum;
+    }
+}
