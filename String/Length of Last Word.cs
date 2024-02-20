ac7101fbce25358c4ba0c05a837975a0436a46bb
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-20 15:28:24 +0500

    58nd problem added

---
+++ String/Length of Last Word.cs
@@ -0,0 +1,12 @@
+﻿/// 58.  https://leetcode.com/problems/length-of-last-word/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+namespace String;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        return s.Trim().Split()[^1].Length;
+    }
+}
