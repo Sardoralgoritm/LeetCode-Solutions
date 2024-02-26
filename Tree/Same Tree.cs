258223248f613e5c1ec81d17a8ec1bce27949af8
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-26 17:52:25 +0500

    Add 100th problem

---
+++ Tree/Same Tree.cs
@@ -0,0 +1,32 @@
+﻿/// 100.  https://leetcode.com/problems/same-tree/?envType=daily-question&envId=2024-02-26
+
+namespace Tree;
+
+//Definition for a binary tree node.
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+ 
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null) return true;
+
+        if (p == null || q == null) return false;
+
+        if (p.val == q.val)
+        {
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+
+        return false;
+    }
+}
