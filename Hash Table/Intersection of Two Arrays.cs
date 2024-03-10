bd5b072c53d2fddca93cc919287b316907f83d07
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-03-10 12:42:41 +0500

    349nd problem added

---
+++ Hash Table/Intersection of Two Arrays.cs
@@ -0,0 +1,13 @@
+﻿/// 349 https://leetcode.com/problems/intersection-of-two-arrays/description/?envType=daily-question&envId=2024-03-10
+
+namespace Hash_Table;
+
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var set1 = nums1.ToHashSet();
+        var set2 = nums2.ToHashSet();
+        return set1.Intersect(set2).ToArray();
+    }
+}
